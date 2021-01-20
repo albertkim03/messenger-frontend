95e29dfbf6f06387efff3bbf55e59fca8d8de9ce
Emily Luxa <emilyluxa@gmail.com>
2021-01-20 17:48:25 +1100

    User remove dialog

---
+++ src/components/Admin/UserRemoveDialog.js
@@ -0,0 +1,117 @@
+import React from "react";
+import axios from 'axios';
+import {
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  Button,
+  TextField,
+  Grid,
+  FormControlLabel,
+  RadioGroup,
+  Radio,
+  MenuItem,
+  Select,
+  InputLabel,
+} from "@material-ui/core";
+import AuthContext from "../../AuthContext";
+
+function UserRemoveDialog({ children, ...props }) {
+
+  const [open, setOpen] = React.useState(false);
+  const [users, setUsers] = React.useState([]);
+  const [selectedUser, setSelectedUser] = React.useState('');
+  const token = React.useContext(AuthContext);
+
+  function fetchUserData() {
+    axios
+      .get('/users/all', {
+        params: {
+          token,
+        },
+      })
+      .then(({ data }) => {
+        setUsers(data['users']);
+      })
+      .catch((err) => { });
+  }
+
+  React.useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const handleUserSelect = event => {
+    const newUserId = parseInt(event.target.value, 10);
+    setSelectedUser(newUserId);
+  };
+
+  function handleClickOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!event.target[0].value) return;
+
+    const u_id = parseInt(event.target[0].value, 10);
+
+    axios
+      .post(`/admin/user/remove`, { data: { token, u_id } })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => { });
+  }
+
+  return <>
+    <div onClick={handleClickOpen}>
+      {children}
+    </div>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="form-dialog-title"
+    >
+      <DialogTitle id="form-dialog-title">Remove Users</DialogTitle>
+      <form onSubmit={handleSubmit}>
+        <DialogContent>
+          <DialogContentText>
+            Select a user below to remove
+          </DialogContentText>
+          <Grid
+            container
+            spacing={2}
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item xs={12}>
+              <Select style={{ width: "100%" }} id="u_id" onChange={handleUserSelect} value={selectedUser}>
+                {users.map((d, idx) => {
+                  return <MenuItem key={d.u_id} value={d.u_id}>{d.name_first} {d.name_last}</MenuItem>
+                })}
+              </Select>
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} type="submit" color="primary">
+            Remove
+          </Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  </>;
+}
+
+export default UserRemoveDialog;
--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -3,13 +3,14 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import SetUserPermissionsDialog from './SetUserPermissionsDialog';
+import UserRemoveDialog from './UserRemoveDialog';
 
 export default function Admin() {
 
   const [open, setOpen] = React.useState(false);
   const buttonRef = React.useRef();
 
-  const handleClick =(event) => setOpen(true);
+  const handleClick = (event) => setOpen(true);
   const handleClose = () => setOpen(false);
 
   return (
@@ -31,8 +32,11 @@ export default function Admin() {
         onClose={handleClose}
       >
         <SetUserPermissionsDialog>
-            <MenuItem onClick={handleClose}>Set User Permissions</MenuItem>
+          <MenuItem onClick={handleClose}>Set User Permissions</MenuItem>
         </SetUserPermissionsDialog>
+        <UserRemoveDialog>
+          <MenuItem onClick={handleClose}>Remove Users</MenuItem>
+        </UserRemoveDialog>
         <MenuItem onClick={handleClose}>Close</MenuItem>
       </Menu>
     </div>
