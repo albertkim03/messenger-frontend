52d337b7ad633a3394726d69f4a754d0aced81b5
Emily Luxa <emilyluxa@gmail.com>
2021-03-07 21:02:08 +1100

    Build

---
+++ prebundle/precache-manifest.0cd22e8eb09082cd73309778d3378b04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642d3a75a7b6df3bc0cab1e318d74d17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c8df07dd3c5c8e93b56",
+    "url": "/static/css/2.6010d4dc.chunk.css"
+  },
+  {
+    "revision": "9b62192840074ab7d8c3",
+    "url": "/static/css/main.c8e4eb10.chunk.css"
+  },
+  {
+    "revision": "7c8df07dd3c5c8e93b56",
+    "url": "/static/js/2.d983363d.chunk.js"
+  },
+  {
+    "revision": "9b62192840074ab7d8c3",
+    "url": "/static/js/main.742093d6.chunk.js"
+  },
+  {
+    "revision": "a2806f7ab0fa17a92097",
+    "url": "/static/js/runtime~main.dc128e9d.js"
+  }
+]);
